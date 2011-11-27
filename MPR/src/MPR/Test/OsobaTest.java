213bbd4e3aabf2f2226df469dcc45fd934f9e383
s9018zao <rafal.cherkowski1@wp.pl>
2011-11-27 14:38:01 +0100

    junit

---
+++ MPR/src/MPR/Test/OsobaTest.java
@@ -0,0 +1,94 @@
+package MPR.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OsobaTest {
+
+	@Test
+	public void testOsoba() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWypiszGazety() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWypiszKsiazki() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWypiszGry() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDodajKsiazki() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDodajGazety() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDodajGry() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testZnajdz() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSzukaj() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPokaz() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSzukajAutora() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testZmienGazete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testZmienKsiazke() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testZmienGre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsunKsiazka() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsunGazeta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsunGra() {
+		fail("Not yet implemented");
+	}
+
+}
